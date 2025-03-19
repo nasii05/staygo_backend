d3614672c1da7ee8ce92004c6860b956db3a029a
nasii05 <iamnasih@gmail.com>
2025-03-19 11:13:47 +0530

    register user API

---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const router = express.Router();
+
+// Register User
+router.post("/register", async (req, res)=> {
+    try{
+        const { name, email, password} = req.body;
+        const userExists = await User.findOne({email});
+        if(userExists) return res.status(400).json({message: "User already exists", status: res.status(400), error: true});
+
+        const user = await User.create({ name, email, password});
+        res.status(201).json({message: "User Registered Successfully", status: res.status(201), erorr: false})
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+})
